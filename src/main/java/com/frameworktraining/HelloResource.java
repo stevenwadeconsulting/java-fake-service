d519dc2cac7429478211c18f4d0541e2e00d149f
Will Varney <github@willvrny.co.uk>
2020-02-08 14:06:15 +0000

    Added configuration via both environment variables and configuration file.

--- src/main/java/com/frameworktraining/HelloResource.java
+++ src/main/java/com/frameworktraining/HelloResource.java
@@ -1,16 +1,22 @@
 package com.frameworktraining;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 
 @Path("/hello")
 public class HelloResource {
 
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "Hello " + name.orElse("world!");
     }
 }
